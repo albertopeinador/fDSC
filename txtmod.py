e0c7d2e07a07da839af5352a53e6974f01261c26
albertopeinador <alb.peiandor@gmail.com>
2024-09-19 23:10:32 +0200

    Some work done, but basically broken (much like myself)...

--- txtmod.py
+++ txtmod.py
@@ -1,4 +1,6 @@
 import re
+import os
+
 def modify_text_file(file_path):
     # Step 1: Read the entire file
     with open(file_path, 'r') as file:
@@ -19,3 +21,42 @@ def modify_text_file(file_path):
         file.writelines(modified_lines)
     
     print(f"Modified {file_path}.")
+
+
+def get_files_dict(main_dir):
+    temps = {}
+    '''    for i in os.listdir(main_dir):
+        match = re.search(r'_(minus )?(\d+)\s*degree(_ref)?', i)
+        print (match)
+        if match:
+            number_str = match.group(2)
+            number = int(number_str)
+            if match.group(1):
+                number = -number
+            if number not in temps:
+                temps[number] = [None, None]
+            if match.group(3):
+                temps[number][1] = i
+            else:
+                # Otherwise, place it in the first slot (index 0)
+                temps[number][0] = i
+    '''
+    for i in os.listdir(main_dir):
+        match = re.search(r'_(minus )?(\d+)\s*degree(_ref)?', i)
+        if match:
+            number_str = match.group(2)
+            number = int(number_str)
+            if match.group(1):
+                number = -number
+            if number not in temps:
+                temps[number] = [None, None]
+            if match.group(3):
+                temps[number][1] = i
+            else:
+                # Otherwise, place it in the first slot (index 0)
+                temps[number][0] = i
+    return temps
+
+
+#print(os.listdir('/Users/albertopeinador/Desktop/fDSC/90816 (Bulk 80kDa)/Annealing30min2kto260'))
+print (get_files_dict('/Users/albertopeinador/Desktop/fDSC/90816 (Bulk 80kDa)/Annealing30min2kto260'))
