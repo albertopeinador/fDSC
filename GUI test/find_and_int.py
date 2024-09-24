df18fd83cd056d99aec1a82c6035bac687b32886
Alberto Peinador <a.peinador@udc.es>
2024-09-24 15:37:23 +0200

    added integral function (not tested)

--- GUI test/find_and_int.py
+++ GUI test/find_and_int.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import scipy.integrate as sc
 
 def find_int_region(df_tuple, thold, y_name):
@@ -6,6 +5,7 @@ def find_int_region(df_tuple, thold, y_name):
     max_diff_index = dif.idxmax().values[0]
     left_index = max_diff_index
     right_index = max_diff_index
+
     while left_index > 0 and dif.iloc[left_index].values[0] > thold:
         left_index -= 1
 
@@ -15,3 +15,7 @@ def find_int_region(df_tuple, thold, y_name):
     int_data = df_tuple[0].iloc[left_index:right_index + 1] - df_tuple[1].iloc[left_index:right_index + 1]
 
     return int_data
+
+def integ(df, y_name, x_name):
+    integral = sc.trapz(df[y_name], df[x_name])
+    return integral
