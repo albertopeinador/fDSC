0f31ff599d02c7980c055652bc171cf38ef69517
Alberto Peinador <a.peinador@udc.es>
2024-09-24 17:01:13 +0200

    Tried to implement integration. Full of errors still.

--- GUI test/find_and_int.py
+++ GUI test/find_and_int.py
@@ -1,21 +1,22 @@
-import scipy.integrate as sc
+import numpy as np
+
 
 def find_int_region(df_tuple, thold, y_name):
     dif = df_tuple[0][y_name] - df_tuple[1][y_name]
-    max_diff_index = dif.idxmax().values[0]
+    max_diff_index = dif.idxmax()
     left_index = max_diff_index
     right_index = max_diff_index
-
-    while left_index > 0 and dif.iloc[left_index].values[0] > thold:
+    while left_index > 0 and dif.iloc[left_index] > thold:
         left_index -= 1
 
-    while (right_index < len(df_tuple[0][y_name]) - 1 or right_index < len(df_tuple[1][y_name])) and dif.iloc[right_index].values[0] > thold:
+    while right_index < len(dif) - 1 and dif.iloc[right_index] > thold:
         right_index += 1
 
-    int_data = df_tuple[0].iloc[left_index:right_index + 1] - df_tuple[1].iloc[left_index:right_index + 1]
-
-    return int_data
+    int_data = df_tuple[0][y_name].iloc[left_index:right_index + 1] - df_tuple[1][y_name].iloc[left_index:right_index + 1]
+    int_data.to_frame()
+    int_data['t'] =  df_tuple[0]['t'].iloc[left_index:right_index + 1]
+    return int_data, left_index, right_index
 
 def integ(df, y_name, x_name):
-    integral = sc.trapz(df[y_name], df[x_name])
+    integral = np.trapz(df[y_name], df[x_name])
     return integral
--- GUI test/main.py
+++ GUI test/main.py
@@ -3,6 +3,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import filehandler as tx
 import file_loader as ld
+import find_and_int as fai
+
 
 #if 'slider_delta' not in st.session_state:
 #    st.session_state['slider_delta'] = 0.
@@ -13,6 +15,8 @@ def update_slider_value():
 
 fig, ax1= plt.subplots(1, 1, sharex=False, sharey=True)
 fig.set_figheight(16)
+fig2, ax2= plt.subplots(1, 1, sharex=False, sharey=True)
+fig2.set_figheight(9)
 
 ctr_panel, graf = st.columns(2)
 
@@ -21,6 +25,7 @@ with ctr_panel:
     load_cutoff = st.slider('cutoff', min_value=2, max_value=100)
     margin_step = st.slider('margin_step', min_value=0, max_value=100) / 100
     eje_x = st.selectbox('x axis', ['Tr', 'Ts', 't'])
+    int_dif_th = st.slider('integral threshold', min_value=0, max_value=100) / 1000
 
 try:
     
@@ -33,6 +38,11 @@ try:
     big_data = ld.load_files(folder_name, temps, file_dict, load_cutoff)
     margin = margin_step * (float(big_data[0][1]['Heat Flow'].max()) - float(big_data[0][1]['Heat Flow'].min()))
 
+    int_regs = {}
+    ints = []
+    lims = {}
+
+    #st.pyplot(fig2)
 
     with ctr_panel:
         mod = st.checkbox ('Modify overlap')
@@ -47,11 +57,19 @@ try:
 
     for i in range(len(temps)):
         big_data[i][0]['Heat Flow'] += (st.session_state[temps[i]] / 10) * big_data[i][0]['Heat Flow'].max()
-
+        regs, left, right = fai.find_int_region(big_data[i], int_dif_th*(big_data[i][0]['Heat Flow'] - big_data[i][1]['Heat Flow']).max(), 'Heat Flow')
+        lims[temps[i]] = (left, right)
+        int_regs[temps[i]] = regs
+        ints.append(fai.integ(int_regs[temps[i]], 'Heat Flow', 't'))
+        ax2.plot(temps[i], ints[i], 'ks')
 
     if mod==True:
+        dif = big_data[temps.index(Ta)][0]['Heat Flow'][lims[Ta][0]:lims[Ta][1]] - big_data[temps.index(Ta)][1]['Heat Flow'][lims[Ta][0]:lims[Ta][1]]
         big_data[temps.index(Ta)][0].plot(x = eje_x, y = 'Heat Flow', ax = ax1, legend=False, style = '#2ca50b')
         big_data[temps.index(Ta)][1].plot(x = eje_x, y = 'Heat Flow', ax = ax1, legend=False, style = '#0886b9', linewidth = 1.6)
+        ax1.axvline(x=big_data[temps.index(Ta)][0][eje_x][lims[Ta][0]], color='r', linestyle='--')
+        ax1.axvline(x=big_data[temps.index(Ta)][0][eje_x][lims[Ta][1]], color='r', linestyle='--')
+        dif.plot(x = eje_x, y = 'Heat Flow', ax = ax1, style = 'r')
     else:
         for i in range (1, len(big_data)):
             dif = abs(big_data[i][0]['Heat Flow'] - big_data[i-1][0]['Heat Flow']).max()
@@ -73,8 +91,6 @@ try:
     
     with graf:
         st.pyplot(fig)
-
-    print (st.session_state['-80'])
 
 
 except FileNotFoundError:
