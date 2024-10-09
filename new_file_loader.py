7b119fc65bb61262cf18075f94abc3ec038ef8d0
Alberto Peinador <a.peinador@udc.es>
2024-10-09 16:36:27 +0200

    Should work now... Touch wood

--- new_file_loader.py
+++ new_file_loader.py
@@ -15,6 +15,15 @@ def load_files(upload_files, cutoff):
                 mod_ref = fh.modify_text_file(bytes_data_ref, cutoff)
 
                 big_data[t] = (pd.read_csv(StringIO(mod), sep = ',', encoding = 'latin1', skipinitialspace = True), pd.read_csv(StringIO(mod_ref), sep = ',', encoding = 'latin1', skipinitialspace = True))
-            else:
-                continue
+            elif dict[t][0] == None:
+                bytes_data_ref = dict[t][1].read().decode('latin1')
+                mod_ref = fh.modify_text_file(bytes_data_ref, cutoff)
+
+                big_data[t] = (None, pd.read_csv(StringIO(mod_ref), sep = ',', encoding = 'latin1', skipinitialspace = True))
+            elif dict[t][1] == None:
+                bytes_data_ref = dict[t][0].read().decode('latin1')
+                mod_ref = fh.modify_text_file(bytes_data_ref, cutoff)
+
+                big_data[t] = (pd.read_csv(StringIO(mod_ref), sep = ',', encoding = 'latin1', skipinitialspace = True), None)
+
     return temps, big_data
