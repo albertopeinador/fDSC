7c8b605aac310d4aff9b39793ca1276b9d9a6da2
Alberto Peinador <a.peinador@udc.es>
2024-10-09 14:12:22 +0200

    cleanup

--- new_filehandler.py
+++ new_filehandler.py
@@ -17,13 +17,13 @@ def modify_text_file(file_content, start_cutoff):
     modified_lines = [line.replace(',', '.') for line in lines]
     modified_lines = [re.sub(r' {2,}', ',', line) for line in modified_lines]
     modified_lines = [re.sub(r'\t{1,}', ',', line) for line in modified_lines]
-
+    #Delte first , in case there is one
     modified_lines = [line.lstrip(',') for line in modified_lines]
     # Step 4: Write the modified lines back to the file
     processed_content = "\n".join(modified_lines)
     return processed_content
     
-    #print(f"Modified {file_path}.")
+
 
 
 def files_to_dict(files_upload):
@@ -49,16 +49,3 @@ def files_to_dict(files_upload):
                     temps_files[number][0] = file
     return temps, temps_files
 
-'''
-def load_files(main_dir, temps, temp_dict, cutoff):
-    big_data = []
-    for i in temps:
-        abs_path = os.path.join(str(os.getcwd()), main_dir, temp_dict[i][0])
-        abs_path_ref = os.path.join(str(os.getcwd()) , main_dir , temp_dict[i][1])
-        modify_text_file(abs_path, cutoff)
-        modify_text_file(abs_path_ref, cutoff)
-        df = pd.read_csv(abs_path, sep = '\t', encoding = 'latin1')
-        df_ref = pd.read_csv(abs_path_ref, sep = '\t', encoding = 'latin1')
-        big_data.append((df, df_ref))
-    return big_data
-    '''
